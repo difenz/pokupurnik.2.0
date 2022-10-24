d63891b1463372a4d4fd739a5f25f9999c60b8a8
German Novikov <difenz1k@gmail.com>
2022-10-25 01:31:49 +0300

    Update main.js

--- main.js
+++ main.js
@@ -101,4 +101,11 @@ function func() {
     }
 
 
+    document.addEventListener("touchmove", function(event) {
+        event = event.originalEvent || event;
+        if (event.scale !== 1) {
+            event.preventDefault();
+        }
+    })
+
 // made by german novikov
