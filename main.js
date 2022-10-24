ee88814808c8691c8f4bf65c0c9db8286375d707
German Novikov <difenz1k@gmail.com>
2022-10-24 20:13:59 +0300

    v2.0.1
    Added 200 bill
    Added cash input

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 const counter50 = document.getElementById("counter50")
 const counter100 = document.getElementById("counter100")
+const counter200 = document.getElementById("counter200")
 const counter500 = document.getElementById("counter500")
 const counter1000 = document.getElementById("counter1000")
 const counter5000 = document.getElementById("counter5000")
 const counterBtn = document.querySelectorAll(".btn")
 const counterBtnHundred = document.querySelectorAll(".hundred")
+const counterBtnTwoHundred = document.querySelectorAll(".twohundred")
 const counterBtnFiveHundred = document.querySelectorAll(".fivehundred")
 const counterBtnThousand = document.querySelectorAll(".thousand")
 const counterBtnFiveThousand = document.querySelectorAll(".fivethousand")
 
 let result50 = 0;
 let result100 = 0;
+let result200 = 0;
 let result500 = 0;
 let result1000 = 0;
 let result5000 = 0;
@@ -36,6 +39,17 @@ counterBtnHundred.forEach((el, index) => {
             result100++;
         }
         counter100.textContent = result100;
+    });
+});
+
+counterBtnTwoHundred.forEach((el, index) => {
+    el.addEventListener('click', () => {
+        if(index === 0 && result200 > 0) {
+            result200--;
+        } else if(index === 1) {
+            result200++;
+        }
+        counter200.textContent = result200;
     });
 });
 
@@ -76,11 +90,12 @@ counterBtnFiveThousand.forEach((el, index) => {
 function func() {
     resultfifty = parseInt(result50) * 50;
     resulthundred = parseInt(result100) * 100;
+    resulttwohundred = parseInt(result200) * 200;
     resultfivehundred = parseInt(result500) * 500;
     resultthousand = parseInt(result1000) * 1000;
     resultfivethousand = parseInt(result5000) * 5000;
     let num1 = Number(document.getElementById("num1").value);
-    document.getElementById("result").innerHTML = parseInt(num1) + parseInt(resultfifty) + parseInt(resulthundred) + parseInt(resultfivehundred) + parseInt(resultthousand) + parseInt(resultfivethousand);
+    document.getElementById("result").innerHTML = parseInt(num1) + parseInt(resultfifty) + parseInt(resulthundred) + parseInt(resulttwohundred) + parseInt(resultfivehundred) + parseInt(resultthousand) + parseInt(resultfivethousand);
     let pc = Number(document.getElementById('pc').value);
     document.getElementById("pcresult").innerHTML = "В ПК: " + pc;
     }
